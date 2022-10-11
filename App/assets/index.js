26736e9eea611bb1cb0f0ca953c733283dd70c1d
Pokilsann <rizkypria09@gmail.com>
2022-10-11 11:17:37 +0700

    add

---
+++ App/assets/index.js
@@ -0,0 +1,8 @@
+import L_peduli from "./img/logo-with-text1.png";
+import kpc from "./img/logo_kpcpen.png";
+import kominfo from "./img/logo_kominfo.png";
+import kemenkes from "./img/logo_kemenkes.png";
+import bumn from "./img/logo_bumn.png";
+//import  from "./img/";
+
+export { L_peduli, kpc, kominfo, kemenkes, bumn };
