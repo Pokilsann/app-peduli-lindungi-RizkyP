95d11487ba5b5579d5c82f5c751aff1043260031
Pokilsann <rizkypria09@gmail.com>
2022-10-11 11:52:20 +0700

    edit

--- App/assets/index.js
+++ App/assets/index.js
@@ -4,5 +4,6 @@ import kominfo from "./img/logo_kominfo.png";
 import kemenkes from "./img/logo_kemenkes.png";
 import bumn from "./img/logo_bumn.png";
 //import  from "./img/";
+import logokotak from "./img/logo-top.png";
 
-export { L_peduli, kpc, kominfo, kemenkes, bumn };
+export { L_peduli, kpc, kominfo, kemenkes, bumn, logokotak };
